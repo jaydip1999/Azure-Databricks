7ab48c24756f247cdd72ab0a2be31eabf47e11f9
dobariyajaydip02@gmail.com <dobariyajaydip02@gmail.com>
2023-01-12 21:14:24 +0000

    init

---
+++ 01 Create Single Column Spark Dataframe using List.py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+ages_list=[21,23,18,41,32]
+
+# COMMAND ----------
+
+type(ages_list)
+
+# COMMAND ----------
+
+help(spark.createDataFrame)
+
+# COMMAND ----------
+
+spark.createDataFrame(ages_list,)
+
+# COMMAND ----------
+
+spark.createDataFrame(ages_list,'int')
+
+# COMMAND ----------
+
+from pyspark.sql.types import IntegerType
+
+# COMMAND ----------
+
+spark.createDataFrame(ages_list,IntegerType())
+
+# COMMAND ----------
+
+names_list=['Jaydip','Bhavik','Dhaval']
+
+# COMMAND ----------
+
+spark.createDataFrame(ages_list,'string')
+
+# COMMAND ----------
+
+from pyspark.sql.types import StringType
+
+# COMMAND ----------
+
+spark.createDataFrame(ages_list,StringType())
+
+# COMMAND ----------
+
+
